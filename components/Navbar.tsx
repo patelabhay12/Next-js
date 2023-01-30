34e406dee0f2414e757aae7b97b6057345a9b7d2
patelabhay12 <0201csds124@niet.co.in>
2023-01-30 10:23:49 +0530

    update

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,15 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import Image from 'next/image';
 import Link from 'next/link';
-import { AiOutlineShoppingCart, AiFillCloseCircle } from 'react-icons/ai';
-;
+import { AiOutlineShoppingCart, AiFillCloseCircle, AiFillPlusCircle, AiFillMinusCircle } from 'react-icons/ai';
+import { BsFillBagCheckFill } from 'react-icons/bs';
 
-const Navbar = () => {
+const Navbar = ({ cart, addToCart, removeFromCart, clearCart, subTotal }) => {
+    // console.log(cart, addToCart, removeFromCart, clearCart, subTotal);
 
 
     const ref = useRef(null);
-    
+
     // useEffect(() => {
     //     console.log("Some thing", ref.current.className);
     //     if (ref.current.classList.contains('translate-x-full')) {
@@ -20,17 +21,17 @@ const Navbar = () => {
     //         ref.current.classList.add('translate-x-full');
     //     }
     // }, []);
-    
+
     const toggleCart = () => {
-            console.log("Some thing", ref.current.className);
-            if (ref.current.classList.contains('translate-x-full')) {
-                ref.current.classList.remove('translate-x-full');
-                ref.current.classList.add('translate-x-0');
-            }
-            else if (!ref.current.classList.contains('translate-x-full')) {
-                ref.current.classList.remove('translate-x-0');
-                ref.current.classList.add('translate-x-full');
-            }
+
+        if (ref.current.classList.contains('translate-x-full')) {
+            ref.current.classList.remove('translate-x-full');
+            ref.current.classList.add('translate-x-0');
+        }
+        else if (!ref.current.classList.contains('translate-x-full')) {
+            ref.current.classList.remove('translate-x-0');
+            ref.current.classList.add('translate-x-full');
+        }
     }
 
 
@@ -48,30 +49,33 @@ const Navbar = () => {
                     <Link href={'/mugs'} legacyBehavior><a><li>Mugs</li></a></Link>
                 </ul>
             </div>
-            <div onClick={toggleCart} className="cart absolute right-0 top-4 mx-5">
+            <div onClick={toggleCart} className="cart cursor-pointer absolute right-0 top-4 mx-5">
                 <AiOutlineShoppingCart className='text-xl md:text-3xl' />
             </div>
 
-            <div ref={ref} onClick={toggleCart} className="sideCart absolute top-0 right-0 bg-pink-100 p-10 transform transition-transform translate-x-full rounded-bl-xl">
-                <h2 className='font-bold text-xl'>Shopping Cart</h2>
-                <span  className='absolute top-5 right-3 cursor-pointer text-2xl text-pink-400'><AiFillCloseCircle /></span>
-                <ol>
-                    <li className='flex flex-col space-y-5'>
-                        <span>Tshirt - Wear The Code</span>
-                        <span>Tshirt - Wear The Code</span>
-                        <span>Tshirt - Wear The Code</span>
-                        <span>Tshirt - Wear The Code</span>
-                        <span>Tshirt - Wear The Code</span>
-                        <span>Tshirt - Wear The Code</span>
-                        <span>Tshirt - Wear The Code</span>
-                        <span>Tshirt - Wear The Code</span>
-                        <span>Tshirt - Wear The Code</span>
-                        <span>Tshirt - Wear The Code</span>
-                    </li>
+            <div ref={ref} className="sideCart z-10 w-72 h-full absolute top-0 right-0 bg-pink-100 py-10 px-8 transform transition-transform translate-x-full rounded-bl-xl">
+                <h2 className='font-bold text-xl text-center'>Shopping Cart</h2>
+                <span onClick={toggleCart} className='absolute top-5 right-3 cursor-pointer text-2xl text-pink-400'><AiFillCloseCircle /></span>
+                <ol className='list-decimal font-semibold' >
+                    {Object.keys(cart).length == 0 && <div className='my-4 font-semibold'>Your Cart is Empty!</div>}
+                    {Object.keys(cart).map((item: any, index: React.Key | null | undefined) => {
+                        return (
+                            <li className='f' key={index}>
+                                <div className="item flex my-5">
+
+                                    <div className='w-2/3 font-semibold'>{cart[item].name}</div>
+                                    <div className='w-1/3 font-semibold flex items-center justify-center text-lg'><AiFillMinusCircle className='cursor-pointer text-pink-500 plus' onClick={() => { removeFromCart(item, 1, cart[item].price, cart[item].name, cart[item].size, cart[item].variant) }} /><span className='mx-2'>{cart[item].qty}</span><AiFillPlusCircle onClick={() => { addToCart(item, 1, cart[item].price, cart[item].name, cart[item].size, cart[item].variant) }} className='cursor-pointer text-pink-500 minus' /></div>
+                                </div>
+                            </li>
+                        )
+                    })}
+
                 </ol>
+                <div className='flex'>
+                    <button className="flex  mr-2 text-white bg-pink-400 border-1 py-2 px-2 focus:outline-1 hover:bg-pink-500 rounded text-sm"><BsFillBagCheckFill className='m-1' />CheckOut</button>
+                    <button onClick={clearCart} className="flex  mr-2 text-white bg-pink-400 border-1 py-2 px-2 focus:outline-1 hover:bg-pink-500 rounded text-sm">Clear Cart</button>
+                </div>
             </div>
-
-
         </div>
     )
 }
